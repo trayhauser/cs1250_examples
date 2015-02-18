781acffff938beee8c7a277cf2da3619c3a53582
George Gerules <gwgkt2@mail.umsl.edu>
2015-02-18 14:40:28 -0600

    added leagal identifiers

---
+++ ch2/leagalidentifiers.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+// which identifiers are incorrect?
+
+int main()
+{
+  int _a = 0;
+  int .a = 0;
+  int $a = 0;
+
+   
+  return 0;
+}
+
