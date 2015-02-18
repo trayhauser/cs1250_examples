85e2fc157e0e4016a0d0f33922c3bfd0a29d502c
Gerules <gwgkt2@delmar.umsl.edu>
2015-02-18 14:35:57 -0600

    added num_not_num

---
+++ ch2/num_not_num.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+// when numbers are not numbers
+
+int main()
+{
+  char cV = '0';
+  string str0 = "0"; 
+  string str1 = "1"; 
+  
+  cout << cV - 1 << " " << str0 + str1 << endl; 
+  return 0;
+}
+
